44fef985c8ba876d92938d0b65a609166513595e
Mahm0udali <el7oda.official@gmail.com>
2025-12-25 10:38:24 +0200

    update dashbord

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
-document.getElementById('imageInput').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    const previewImage = document.getElementById('previewImage');
-    
-    if (file) {
-        const reader = new FileReader();
-        
-        reader.onload = function(e) {
-            previewImage.src = e.target.result;
-            previewImage.style.display = 'block';
-        }
-        
-        reader.readAsDataURL(file);
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const msg = document.getElementById("msg");
+    if (username === "admin" && password === "1234") {
+        localStorage.setItem("user", username);
+        window.location.href = "dashbord.html";
     } else {
-        previewImage.style.display = 'none';
+        msg.textContent = "Wrong User or password";
+        msg.style.color = "red";
     }
-    function resizeImage(img, maxWidth, maxHeight) {
-        const canvas = document.createElement('canvas');
-        let width = img.width;
-        let height = img.height;
-        
-        if (width > maxWidth) {
-            height *= maxWidth / width;
-            width = maxWidth;
-        }
-        
-        canvas.width = width;
-        canvas.height = height;
-        
-        const ctx = canvas.getContext('2d');
-        ctx.drawImage(img, 0, 0, width, height);
-        
-        return canvas.toDataURL('image/jpeg');
-    }
-});
+}
